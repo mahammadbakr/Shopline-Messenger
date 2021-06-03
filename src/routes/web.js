c345af9e04ceee6428d746ec76680f791b16312f
Mohammed Bakr <49376835+mahammadbakr@users.noreply.github.com>
2021-06-03 16:56:05 +0300

    Add files via upload

---
+++ src/routes/web.js
@@ -0,0 +1,15 @@
+const express =require("express");
+const homepageController =require("../controllers/homepageController");
+const chatBotController =require("../controllers/chatBotController");
+
+let router = express.Router();
+
+let initWebRoutes = (app)=> {
+    router.get("/", homepageController.getHomepage);
+    router.get("/webhook", chatBotController.getWebhook);
+    router.post("/webhook", chatBotController.postWebhook);
+
+    return app.use("/", router);
+};
+
+module.exports = initWebRoutes;
